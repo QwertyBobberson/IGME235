f3d2dcf03f24b34f8f6f9aa82d603c06cf7eb492
QwertyBobberson <cebrhim@gmail.com>
2022-12-09 21:18:11 -0500

    Project3

--- Project3/js/classes.js
+++ Project3/js/classes.js
@@ -1,41 +1,84 @@
-class Circle extends PIXI.Graphics
+class Sprite extends PIXI.Sprite
 {
-	constructor(radius, color=0xff0000, x=0, y=0)
+	constructor(x, y, img)
 	{
-		super();
-		this.beginFill(color);
-		this.drawCircle(0, 0, radius);
-		this.endFill();
+		let texture = app.loader.resources[img].texture;
+        super(texture);
+        this.anchor.set(.5, .5);
+        this.scale.set(0.1);
 		this.x = x;
 		this.y = y;
-		this.radius = radius;
-		this.fwd = getRandomUnitVector();
-		this.speed = 50;
-		this.isAlive = true;
-	}
-
-	move(dt=1/60)
-	{
-		this.x += this.fwd.x * this.speed * dt;
-		this.y += this.fwd.y * this.speed * dt;
-	}
-
-	reflectX()
-	{
-		this.fwd.x *= -1;
-	}
-	reflectY()
-	{
-		this.fwd.y *= -1;
 	}
 }
 
-class Sheep extends Circle
+//Handles movement and displaying scores
+//X and Y: start positions (numbers)
+//Img: path to the img for this agent (string)
+//hasScore: Weather or not the agent has a score to display (bool)
+//Title: title to display infront of score (string)
+//ScoreX: X coordinate of score for this agent (number)
+class Agent
 {
+	constructor(x = 300, y = 300, img, hasScore = false, title = "", scoreX = 0)
+	{
+		this.sprite = new Sprite(x, y, img);
+		this.title = title;
+		//
+		if(hasScore)
+		{
+			this.score = 0;
+			this.scoreText = new PIXI.Text(title + " Score: 0");
+			this.scoreText.style = new PIXI.TextStyle({
+				fill: 0xffffff,
+				fontSize: 24,
+				fontFamily: "Futura",
+				stroke: 0x0000ff,
+				strokeThickness: 1
+			});
+			this.scoreText.x = scoreX;
+			this.scoreText.anchor.set(scoreX/600, 0);
+		}
+		this.speed = 100;
+		this.fwd = {x: 0, y: 0};
+	}
 
-}
+	//Add amount to score and update textbox
+	increaseScore(amount = 1)
+	{
+		this.score += amount;
+		this.scoreText.text = this.title + " Score: " + this.score;
+	}
 
-class Wolf extends Circle
-{
+	//Move forwards at a speed of speed * speedModifier
+	move(dt=1/60, speedModifier = 1)
+	{
+		this.sprite.x += this.fwd.x * this.speed * speedModifier * dt;
+		this.sprite.y += this.fwd.y * this.speed * speedModifier * dt;
+	}
+
+	//Gets the direction towards a passed in agent
+	//Sets fwd to that direction
+	setFwdTowards(target)
+	{
+		let dir = {x: target.x - this.sprite.x, y: target.y - this.sprite.y};
+		let mag = Math.sqrt(dir.x * dir.x + dir.y * dir.y);
+		this.fwd = {x: dir.x/mag, y: dir.y/mag};
+	}
+
+	//Set forwards towards an agent and move
+	//Ensures the agent stays in bounds
+	moveTowards(target, dt=1/60, speedModifier = 1)
+	{
+		this.setFwdTowards(target);
+
+		if(
+			this.sprite.x + (this.fwd.x * this.speed * speedModifier * dt) > 0 &&
+			this.sprite.x + (this.fwd.x * this.speed * speedModifier * dt) < 600 &&
+			this.sprite.y + (this.fwd.y * this.speed * speedModifier * dt) > 0 &&
+			this.sprite.y + (this.fwd.y * this.speed * speedModifier * dt) < 600 )
+		{
+			this.move(dt, speedModifier);
+		}
+	}
 
 }
--- Project3/js/main.js
+++ Project3/js/main.js
@@ -1,7 +1,8 @@
 "use strict";
 const app = new PIXI.Application({
     width: 600,
-    height: 600
+    height: 600,
+    backgroundColor: 0x114411
 });
 document.body.appendChild(app.view);
 
@@ -11,8 +12,9 @@ const sceneHeight = app.view.height;
 
 app.loader.
     add([
-        "images/spaceship.png",
-        "images/explosions.png"
+        "images/wolf.png",
+        "images/sheep.png",
+        "images/shepherd.png"
     ]);
 app.loader.onProgress.add(e => { console.log(`progress=${e.progress}`) });
 app.loader.onComplete.add(setup);
@@ -22,22 +24,80 @@ let stage;
 
 const speed = 3;
 
-let gameScene;
+let gameScene, menuScene;
 let wolf;
 let sheep;
 let player;
 
 let sheepTarget;
 
-let score;
+let wolfScore;
+let wolfScoreText;
+
+let playerScore;
+let playerScoreText;
+
+let chompNoise = new Howl({
+    src: ['audio/chomp.wav']
+});
+let baaNoise = new Howl({
+    src: ['audio/baa.wav']
+});
 
 function setup()
 {
     stage = app.stage;
-    gameScene = new PIXI.Container();
+    createMenuScene();
+}
 
-    app.ticker.add(gameLoop);
-    startGame();
+function createMenuScene()
+{
+    menuScene = new PIXI.Container();
+    let title = new PIXI.Text("Shepherd!");
+    title.style = new PIXI.TextStyle({
+        fill: 0xffffff,
+        fontSize: 96,
+        fontFamily: "Futura",
+        stroke: 0x0000ff,
+        strokeThickness: 6
+    });
+
+    title.x = 50;
+    title.y = 120;
+
+    let instructions = new PIXI.Text("Save the sheep from the evil wolf!");
+    instructions.style = new PIXI.TextStyle({
+        fill: 0xffffff,
+        fontSize: 36,
+        fontFamily: "Futura",
+        stroke: 0x0000ff,
+        strokeThickness: 3
+    });
+
+    instructions.x = 5;
+    instructions.y = 300;
+
+    let startButton = new PIXI.Text("TO THE SHEEP!");
+    startButton.style = new PIXI.TextStyle({
+        fill: 0x00dddd,
+        fontSize: 48,
+        fontFamily: "Futura"
+    });;
+    startButton.x = 300;
+    startButton.y = sceneHeight - 100;
+    startButton.interactive = true;
+    startButton.buttonMode = true;
+    startButton.on("pointerup", startGame);
+    startButton.on("pointerover", e => e.target.alpha = 0.7);
+    startButton.on("pointerout", e => e.currentTarget.alpha = 1.0);
+    startButton.anchor.set(0.5, 0.5)
+
+
+
+    menuScene.addChild(title);
+    menuScene.addChild(instructions);
+    menuScene.addChild(startButton);
+    stage.addChild(menuScene);
 }
 
 function gameLoop()
@@ -45,53 +105,73 @@ function gameLoop()
     let dt = 1/app.ticker.FPS;
 	if (dt > 1/12) dt=1/12;
 
-    if(rectsIntersect(wolf, sheep))
+    //Check if either the wolf or player is touching the sheep
+    //Award the point to whichever one is
+    //Move the sheep
+    if(rectsIntersect(wolf.sprite, sheep.sprite))
     {
         SetupSheep();
-        score -= 10;
+        wolf.increaseScore();
+        chompNoise.play();
     }
-    if(rectsIntersect(player, sheep))
+    if(rectsIntersect(player.sprite, sheep.sprite))
     {
         SetupSheep();
-        score += 5;
+        player.increaseScore();
+        baaNoise.play();
+    }
+
+    //Allow the player to move more quickly when losing
+    //Player must be faster than wold to compensate for reaction times
+    let speedModifier = (wolf.score/player.score) + 0.5;
+    if(isNaN(speedModifier) || speedModifier < 1.1)
+    {
+        speedModifier = 1.1;
+    }
+
+    if(!isFinite(speedModifier))
+    {
+        speedModifier = 1.5;
     }
 
 	let mousePosition = app.renderer.plugins.interaction.mouse.global;
-    player.fwd = GetFwd(player, mousePosition, speed * 1.2);
-    player.move(dt);
 
-    wolf.fwd = GetFwd(wolf, sheep, speed);
-    wolf.move(dt);
+    //Player moves towards mouse
+    //Wolf moves towards sheep
+    //Sheep moves towards random position
 
-    sheep.fwd = GetFwd(sheep, sheepTarget, speed/2);
-    sheep.move(dt);
+    player.moveTowards(mousePosition, dt, speedModifier);
+    wolf.moveTowards(sheep.sprite, dt);
+    sheep.moveTowards(sheepTarget, dt, 0.5);
 
 }
 
 function SetupSheep()
 {
-    sheep.x =  getRandom(0, 600);
-    sheep.y = getRandom(0, 600);
-    sheepTarget = {x: getRandom(0, 600), y: getRandom(0, 600)};
-}
-
-function GetFwd(agent, target, speed)
-{
-    let dir = {x: target.x - agent.x, y: target.y - agent.y};
-    let mag = Math.sqrt(dir.x * dir.x + dir.y * dir.y)/speed;
-    if(isNaN(mag))
-    {
-        return {x: 0, y: 0};
-    }
-    return {x: dir.x/mag, y: dir.y/mag};
+    //Randomize the sheep's position and target position
+    sheep.sprite.x =  getRandom(0, 500);
+    sheep.sprite.y = getRandom(0, 500);
+    sheepTarget = {x: getRandom(0, 500), y: getRandom(0, 500)};
 }
 
 function startGame()
 {
-    score = 0;
-    wolf = gameScene.addChild(new Wolf(10, 0xff0000, 100, 100));
-    sheep = gameScene.addChild(new Sheep(10, 0x0000ff, 500, 500));
+    gameScene = new PIXI.Container();
+    //Create objects for the player, wolf, and sheep
+    sheep = new Agent(500, 500, "images/sheep.png");
     SetupSheep();
-    player = gameScene.addChild(new Circle(10, 0xffffff, 300, 300));
+    wolf = new Agent(100, 100, "images/wolf.png", true, "Wolf", 600);
+    player = new Agent(300, 300, "images/shepherd.png", true, "Shepherd", 0);
+
+    //Show all agents and hide the menu
+    gameScene.addChild(player.scoreText);
+    gameScene.addChild(player.sprite);
+    gameScene.addChild(wolf.scoreText);
+    gameScene.addChild(wolf.sprite);
+    gameScene.addChild(sheep.sprite);
     stage.addChild(gameScene);
+    menuScene.visible = false;
+
+
+    app.ticker.add(gameLoop);
 }
